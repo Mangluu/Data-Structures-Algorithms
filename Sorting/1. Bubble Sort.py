7e99496e80ba5de9336f38fe2a89a63198888e30
Shivang Gupta <shivangzephyr@gmail.com>
2020-05-18 16:23:49 +0530

    Create 1. Bubble Sort.py

---
+++ Sorting/1. Bubble Sort.py
@@ -0,0 +1,11 @@
+def bubble_sort(arr):
+    for j in range(len(arr)-1, 0, -1):
+        print(arr)
+        for i in range(0, j):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+    return
+
+
+l = [6, 8, 1, 4, 10, 7, 8, 9, 3, 2, 5]
+bubble_sort(l)
