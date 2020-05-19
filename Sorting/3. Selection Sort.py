5dae566a17f562596c00973d09e6096210de0108
Manglu <shivangzephyr@gmail.com>
2020-05-19 17:29:01 +0530

    Selection Sort

---
+++ Sorting/3. Selection Sort.py
@@ -0,0 +1,10 @@
+def selectio_sort(arr):
+    for i in range(len(arr)):
+        print(arr)
+        for j in range(i, len(arr)):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+
+
+l = [6, 8, 1, 4, 10, 7, 8, 9, 3, 2, 5]
+selectio_sort(l)
